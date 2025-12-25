fc86fad46e26c1d550585df80db8340aead45e09
Sujata Halder <sujata.halder@outlook.com>
2025-12-25 20:23:04 +0530

    useState with turnary operator

--- src/Components/pages/Test.jsx
+++ src/Components/pages/Test.jsx
@@ -4,8 +4,7 @@ const Test = () => {
   const[isDark, setIsDark] = useState(false);
   return (
         <>
-        {/*bun run dev
-         Toggle between dark and light mode with a button. Change background and text colors accordingly. */}
+        {/*Toggle between dark and light mode with a button. Change background and text colors accordingly. */}
           <div className={isDark ? 'dark' : 'light'} style={{textAlign: "center", marginTop: "50px"}} >
              <h1 style={{color: isDark ? 'white' : 'black'}}>{isDark ? 'Dark Mode' : 'Light Mode'}</h1>
              <button className='btn btn-danger' onClick={() => setIsDark(!isDark)}>Toggle theme</button>
